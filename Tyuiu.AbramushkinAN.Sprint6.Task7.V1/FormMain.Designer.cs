14153247f28f9694d7db89ad0d27741c905b6908
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-12-14 11:04:20 +0500

    Task7

--- Tyuiu.AbramushkinAN.Sprint6.Task7.V1/FormMain.Designer.cs
+++ Tyuiu.AbramushkinAN.Sprint6.Task7.V1/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tyuiu.AbramushkinAN.Sprint6.Task7.V1
 {
-    partial class Form1
+    partial class FormMain_AAN
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain_AAN));
+            panelTools_AAN = new Panel();
+            buttonAboutProgramm_AAN = new Button();
+            buttonSaveFile_AAN = new Button();
+            buttonDone_AAN = new Button();
+            buttonOpenFile_AAN = new Button();
+            panelCondition_AAN = new Panel();
+            groupboxCondition_AAN = new GroupBox();
+            textBoxCondition_AAN = new TextBox();
+            panelInput_AAN = new Panel();
+            groupBoxInput_AAN = new GroupBox();
+            dataGridViewInput_AAN = new DataGridView();
+            panelOutput_AAN = new Panel();
+            groupBoxOutput_AAN = new GroupBox();
+            dataGridViewOutput_AAN = new DataGridView();
+            openFileDialogTask_AAN = new OpenFileDialog();
+            saveFileDialogMatrix_AAN = new SaveFileDialog();
+            toolTipButton_AAN = new ToolTip(components);
+            panelTools_AAN.SuspendLayout();
+            panelCondition_AAN.SuspendLayout();
+            groupboxCondition_AAN.SuspendLayout();
+            panelInput_AAN.SuspendLayout();
+            groupBoxInput_AAN.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput_AAN).BeginInit();
+            panelOutput_AAN.SuspendLayout();
+            groupBoxOutput_AAN.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutput_AAN).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTools_AAN
+            // 
+            panelTools_AAN.Controls.Add(buttonAboutProgramm_AAN);
+            panelTools_AAN.Controls.Add(buttonSaveFile_AAN);
+            panelTools_AAN.Controls.Add(buttonDone_AAN);
+            panelTools_AAN.Controls.Add(buttonOpenFile_AAN);
+            panelTools_AAN.Dock = DockStyle.Top;
+            panelTools_AAN.Location = new Point(0, 0);
+            panelTools_AAN.Name = "panelTools_AAN";
+            panelTools_AAN.Size = new Size(1184, 100);
+            panelTools_AAN.TabIndex = 0;
+            // 
+            // buttonAboutProgramm_AAN
+            // 
+            buttonAboutProgramm_AAN.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            buttonAboutProgramm_AAN.BackColor = SystemColors.ControlLight;
+            buttonAboutProgramm_AAN.Image = (Image)resources.GetObject("buttonAboutProgramm_AAN.Image");
+            buttonAboutProgramm_AAN.Location = new Point(1050, 3);
+            buttonAboutProgramm_AAN.Name = "buttonAboutProgramm_AAN";
+            buttonAboutProgramm_AAN.Size = new Size(131, 91);
+            buttonAboutProgramm_AAN.TabIndex = 3;
+            toolTipButton_AAN.SetToolTip(buttonAboutProgramm_AAN, "Сведения о программе\r\n");
+            buttonAboutProgramm_AAN.UseVisualStyleBackColor = false;
+            buttonAboutProgramm_AAN.Click += buttonAboutProgramm_Click;
+            buttonAboutProgramm_AAN.MouseEnter += buttonAboutProgramm_MouseEnter;
+            // 
+            // buttonSaveFile_AAN
+            // 
+            buttonSaveFile_AAN.BackColor = SystemColors.ControlLight;
+            buttonSaveFile_AAN.Image = (Image)resources.GetObject("buttonSaveFile_AAN.Image");
+            buttonSaveFile_AAN.Location = new Point(277, 3);
+            buttonSaveFile_AAN.Name = "buttonSaveFile_AAN";
+            buttonSaveFile_AAN.Size = new Size(131, 91);
+            buttonSaveFile_AAN.TabIndex = 2;
+            toolTipButton_AAN.SetToolTip(buttonSaveFile_AAN, "Сохранить обработанную матрицу в указанный файл\r\n");
+            buttonSaveFile_AAN.UseVisualStyleBackColor = false;
+            buttonSaveFile_AAN.Click += buttonSaveFile_Click;
+            buttonSaveFile_AAN.MouseEnter += buttonSaveFile_MouseEnter;
+            // 
+            // buttonDone_AAN
+            // 
+            buttonDone_AAN.BackColor = SystemColors.ControlLight;
+            buttonDone_AAN.Image = (Image)resources.GetObject("buttonDone_AAN.Image");
+            buttonDone_AAN.Location = new Point(140, 3);
+            buttonDone_AAN.Name = "buttonDone_AAN";
+            buttonDone_AAN.Size = new Size(131, 91);
+            buttonDone_AAN.TabIndex = 1;
+            toolTipButton_AAN.SetToolTip(buttonDone_AAN, "Обработать файл по условию\r\n\r\n");
+            buttonDone_AAN.UseVisualStyleBackColor = false;
+            buttonDone_AAN.Click += buttonDone_Click;
+            buttonDone_AAN.MouseEnter += buttonDone_MouseEnter;
+            // 
+            // buttonOpenFile_AAN
+            // 
+            buttonOpenFile_AAN.BackColor = SystemColors.ControlLight;
+            buttonOpenFile_AAN.Image = (Image)resources.GetObject("buttonOpenFile_AAN.Image");
+            buttonOpenFile_AAN.Location = new Point(3, 3);
+            buttonOpenFile_AAN.Name = "buttonOpenFile_AAN";
+            buttonOpenFile_AAN.Size = new Size(131, 91);
+            buttonOpenFile_AAN.TabIndex = 0;
+            toolTipButton_AAN.SetToolTip(buttonOpenFile_AAN, "Выберите файл для обработки\r\n");
+            buttonOpenFile_AAN.UseVisualStyleBackColor = false;
+            buttonOpenFile_AAN.Click += buttonOpenFile_Click;
+            buttonOpenFile_AAN.MouseEnter += buttonOpenFile_MouseEnter;
+            // 
+            // panelCondition_AAN
+            // 
+            panelCondition_AAN.Controls.Add(groupboxCondition_AAN);
+            panelCondition_AAN.Dock = DockStyle.Top;
+            panelCondition_AAN.Location = new Point(0, 100);
+            panelCondition_AAN.Name = "panelCondition_AAN";
+            panelCondition_AAN.Size = new Size(1184, 89);
+            panelCondition_AAN.TabIndex = 1;
+            // 
+            // groupboxCondition_AAN
+            // 
+            groupboxCondition_AAN.Controls.Add(textBoxCondition_AAN);
+            groupboxCondition_AAN.Dock = DockStyle.Fill;
+            groupboxCondition_AAN.Location = new Point(0, 0);
+            groupboxCondition_AAN.Name = "groupboxCondition_AAN";
+            groupboxCondition_AAN.Size = new Size(1184, 89);
+            groupboxCondition_AAN.TabIndex = 0;
+            groupboxCondition_AAN.TabStop = false;
+            groupboxCondition_AAN.Text = "Условие:";
+            // 
+            // textBoxCondition_AAN
+            // 
+            textBoxCondition_AAN.BackColor = SystemColors.Control;
+            textBoxCondition_AAN.BorderStyle = BorderStyle.None;
+            textBoxCondition_AAN.Dock = DockStyle.Fill;
+            textBoxCondition_AAN.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBoxCondition_AAN.Location = new Point(3, 19);
+            textBoxCondition_AAN.Multiline = true;
+            textBoxCondition_AAN.Name = "textBoxCondition_AAN";
+            textBoxCondition_AAN.Size = new Size(1178, 67);
+            textBoxCondition_AAN.TabIndex = 0;
+            textBoxCondition_AAN.Text = resources.GetString("textBoxCondition_AAN.Text");
+            // 
+            // panelInput_AAN
+            // 
+            panelInput_AAN.Controls.Add(groupBoxInput_AAN);
+            panelInput_AAN.Dock = DockStyle.Left;
+            panelInput_AAN.Location = new Point(0, 189);
+            panelInput_AAN.Name = "panelInput_AAN";
+            panelInput_AAN.Size = new Size(584, 372);
+            panelInput_AAN.TabIndex = 2;
+            // 
+            // groupBoxInput_AAN
+            // 
+            groupBoxInput_AAN.Controls.Add(dataGridViewInput_AAN);
+            groupBoxInput_AAN.Dock = DockStyle.Left;
+            groupBoxInput_AAN.Location = new Point(0, 0);
+            groupBoxInput_AAN.Name = "groupBoxInput_AAN";
+            groupBoxInput_AAN.Size = new Size(584, 372);
+            groupBoxInput_AAN.TabIndex = 0;
+            groupBoxInput_AAN.TabStop = false;
+            groupBoxInput_AAN.Text = "Ввод:";
+            // 
+            // dataGridViewInput_AAN
+            // 
+            dataGridViewInput_AAN.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewInput_AAN.Dock = DockStyle.Fill;
+            dataGridViewInput_AAN.Location = new Point(3, 19);
+            dataGridViewInput_AAN.Name = "dataGridViewInput_AAN";
+            dataGridViewInput_AAN.Size = new Size(578, 350);
+            dataGridViewInput_AAN.TabIndex = 0;
+            // 
+            // panelOutput_AAN
+            // 
+            panelOutput_AAN.Controls.Add(groupBoxOutput_AAN);
+            panelOutput_AAN.Dock = DockStyle.Fill;
+            panelOutput_AAN.Location = new Point(584, 189);
+            panelOutput_AAN.Name = "panelOutput_AAN";
+            panelOutput_AAN.Size = new Size(600, 372);
+            panelOutput_AAN.TabIndex = 3;
+            // 
+            // groupBoxOutput_AAN
+            // 
+            groupBoxOutput_AAN.Controls.Add(dataGridViewOutput_AAN);
+            groupBoxOutput_AAN.Dock = DockStyle.Fill;
+            groupBoxOutput_AAN.Location = new Point(0, 0);
+            groupBoxOutput_AAN.Name = "groupBoxOutput_AAN";
+            groupBoxOutput_AAN.Size = new Size(600, 372);
+            groupBoxOutput_AAN.TabIndex = 0;
+            groupBoxOutput_AAN.TabStop = false;
+            groupBoxOutput_AAN.Text = "Вывод:";
+            // 
+            // dataGridViewOutput_AAN
+            // 
+            dataGridViewOutput_AAN.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewOutput_AAN.Dock = DockStyle.Fill;
+            dataGridViewOutput_AAN.Location = new Point(3, 19);
+            dataGridViewOutput_AAN.Name = "dataGridViewOutput_AAN";
+            dataGridViewOutput_AAN.Size = new Size(594, 350);
+            dataGridViewOutput_AAN.TabIndex = 0;
+            // 
+            // openFileDialogTask_AAN
+            // 
+            openFileDialogTask_AAN.FileName = "openFileDialog1";
+            // 
+            // toolTipButton_AAN
+            // 
+            toolTipButton_AAN.ToolTipIcon = ToolTipIcon.Info;
+            // 
+            // FormMain_AAN
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1184, 561);
+            Controls.Add(panelOutput_AAN);
+            Controls.Add(panelInput_AAN);
+            Controls.Add(panelCondition_AAN);
+            Controls.Add(panelTools_AAN);
+            MinimumSize = new Size(1200, 600);
+            Name = "FormMain_AAN";
+            Text = "Спринт 6 | Таск 7 | Вариант 1 | Абрамушкин А.Н.";
+            panelTools_AAN.ResumeLayout(false);
+            panelCondition_AAN.ResumeLayout(false);
+            groupboxCondition_AAN.ResumeLayout(false);
+            groupboxCondition_AAN.PerformLayout();
+            panelInput_AAN.ResumeLayout(false);
+            groupBoxInput_AAN.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput_AAN).EndInit();
+            panelOutput_AAN.ResumeLayout(false);
+            groupBoxOutput_AAN.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOutput_AAN).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panelTools_AAN;
+        private Button buttonAboutProgramm_AAN;
+        private Button buttonSaveFile_AAN;
+        private Button buttonDone_AAN;
+        private Button buttonOpenFile_AAN;
+        private Panel panelCondition_AAN;
+        private GroupBox groupboxCondition_AAN;
+        private TextBox textBoxCondition_AAN;
+        private Panel panelInput_AAN;
+        private GroupBox groupBoxInput_AAN;
+        private DataGridView dataGridViewInput_AAN;
+        private Panel panelOutput_AAN;
+        private GroupBox groupBoxOutput_AAN;
+        private DataGridView dataGridViewOutput_AAN;
+        private ToolTip toolTipButton_AAN;
+        private OpenFileDialog openFileDialogTask_AAN;
+        private SaveFileDialog saveFileDialogMatrix_AAN;
     }
 }
--- Tyuiu.AbramushkinAN.Sprint6.Task7.V1/FormMain.cs
+++ Tyuiu.AbramushkinAN.Sprint6.Task7.V1/FormMain.cs
@@ -1,10 +1,140 @@
+using Tyuiu.AbramushkinAN.Sprint6.Task7.V1.Lib;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Tyuiu.AbramushkinAN.Sprint6.Task7.V1
 {
-    public partial class Form1 : Form
+    public partial class FormMain_AAN : Form
     {
-        public Form1()
+        public FormMain_AAN()
         {
             InitializeComponent();
+            buttonDone_AAN.Enabled = false;
+            buttonSaveFile_AAN.Enabled = false;
+            openFileDialogTask_AAN.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+            saveFileDialogMatrix_AAN.Filter = "Значения, разделённые запятыми(*.csv)|*.csv|Все файлы(*.*)|*.*";
+
         }
+        DataService ds = new DataService();
+        static int columns;
+        static int rows;
+        string openFilePath = "";
+        public static int[,] LoadFromDataFile(string filePath)
+        {
+            string fileData = File.ReadAllText(filePath);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            rows = lines.Length;
+            columns = lines[0].Split(';').Length;
+            int[,] arrayValues = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line_r = lines[i].Split(';');
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayValues[i, j] = Convert.ToInt32(line_r[j]);
+                }
+            }
+            return arrayValues;
+        }
+        private void buttonAboutProgramm_Click(object sender, EventArgs e)
+        {
+            FormAbout formAbout = new FormAbout();
+            formAbout.ShowDialog();
+        }
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            int[,] arrayResult = ds.GetMatrix(openFilePath);
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewOutput_AAN.Rows[r].Cells[c].Value = arrayResult[r,c];
+                }
+            }
+            buttonSaveFile_AAN.Enabled = true;
+        }
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialogMatrix_AAN.FileName = "OutPutFileTask7.csv";
+            saveFileDialogMatrix_AAN.InitialDirectory = Directory.GetCurrentDirectory();
+            saveFileDialogMatrix_AAN.ShowDialog();
+            string path = saveFileDialogMatrix_AAN.FileName;
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) 
+            {
+                File.Delete(path);
+            }
+            int rows = dataGridViewOutput_AAN.RowCount;
+            int columns = dataGridViewOutput_AAN.ColumnCount;
+
+            string str = "";
+            for (int r = 0;r < rows; r++)
+            {
+                for(int c = 0;c < columns; c++)
+                {
+                    if (c != columns - 1)
+                    {
+                        str += dataGridViewOutput_AAN.Rows[r].Cells[c].Value + ";";
+                    }
+                    else
+                    {
+                        str += dataGridViewOutput_AAN.Rows[r].Cells[c].Value;
+                    }
+                }
+                File.AppendAllText(path, str + Environment.NewLine);
+                str = "";
+            }
+
+
+
+
+        }
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogTask_AAN.ShowDialog();
+            string openFilePath = openFileDialogTask_AAN.FileName;
+
+            int[,] arrayValues = new int[rows,columns];
+
+            arrayValues = LoadFromDataFile(openFilePath);
+            dataGridViewInput_AAN.ColumnCount = columns;
+            dataGridViewInput_AAN.RowCount = rows;
+            dataGridViewOutput_AAN.ColumnCount = columns;
+            dataGridViewOutput_AAN.RowCount = rows;
+
+            for (int i = 0; i<columns; i++)
+            {
+                dataGridViewInput_AAN.Columns[i].Width = 25;
+                dataGridViewOutput_AAN.Columns[i].Width = 25;
+            }
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    dataGridViewInput_AAN.Rows[r].Cells[c].Value = arrayValues[r,c];
+                }
+            }
+            buttonDone_AAN.Enabled = true;
+        }
+        private void buttonOpenFile_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipButton_AAN.ToolTipTitle = "Открыть файл";
+        }
+        private void buttonDone_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipButton_AAN.ToolTipTitle = "Запустить программу";
+        }
+        private void buttonSaveFile_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipButton_AAN.ToolTipTitle = "Сохранить в файл";
+        }
+        private void buttonAboutProgramm_MouseEnter(object sender, EventArgs e)
+        {
+            toolTipButton_AAN.ToolTipTitle = "О программе";
+        }
+
     }
 }
